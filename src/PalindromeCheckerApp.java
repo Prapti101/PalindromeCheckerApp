8e4c0bbc87a1b937318a7ce9dd85f56f0a8fd2ab
PraptiPriya <praptipriya486@gmail.com>
2026-02-25 14:29:20 +0530

    UC13:Performance Comparison

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,83 +1,76 @@
 import java.util.*;
 
-interface PalindromeStrategy {
-    boolean check(String input);
-}
-class StackStrategy implements PalindromeStrategy {
-
-    @Override
-    public boolean check(String input) {
-
-        String str = input.replaceAll("\\s+", "").toLowerCase();
-        Stack<Character> stack = new Stack<>();
-        for (char ch : str.toCharArray()) {
-            stack.push(ch);
-        }
-        for (char ch : str.toCharArray()) {
-            if (ch != stack.pop()) {
-                return false;
-            }
-        }
-        return true;
-    }
-}
-class DequeStrategy implements PalindromeStrategy {
-    @Override
-    public boolean check(String input) {
-
-        String str = input.replaceAll("\\s+", "").toLowerCase();
-
-        Deque<Character> deque = new ArrayDeque<>();
-
-        for (char ch : str.toCharArray()) {
-            deque.addLast(ch);
-        }
-        while (deque.size() > 1) {
-            if (!deque.removeFirst().equals(deque.removeLast())) {
-                return false;
-            }
-        }
-
-        return true;
-    }
-}
-class PalindromeContext {
-
-    private PalindromeStrategy strategy;
-
-    public void setStrategy(PalindromeStrategy strategy) {
-        this.strategy = strategy;
-    }
-    public boolean execute(String input) {
-        if (strategy == null) {
-            throw new IllegalStateException("Strategy not set!");
-        }
-        return strategy.check(input);
-    }
-}
-
 public class PalindromeCheckerApp {
+
+    // 1️⃣ Two Pointer Approach
+    public static boolean twoPointer(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right))
+                return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static boolean stackMethod(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray())
+            stack.push(ch);
+
+        for (char ch : str.toCharArray())
+            if (ch != stack.pop())
+                return false;
+
+        return true;
+    }
+
+    public static boolean recursiveMethod(String str) {
+        str = str.replaceAll("\\s+", "").toLowerCase();
+        return recursiveCheck(str, 0, str.length() - 1);
+    }
+
+    private static boolean recursiveCheck(String str, int start, int end) {
+        if (start >= end)
+            return true;
+
+        if (str.charAt(start) != str.charAt(end))
+            return false;
+
+        return recursiveCheck(str, start + 1, end - 1);
+    }
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("Choose Strategy:");
-        System.out.println("1. Stack Strategy");
-        System.out.println("2. Deque Strategy");
-        int choice = sc.nextInt();
-        sc.nextLine();
+
         System.out.print("Enter a string: ");
         String input = sc.nextLine();
-        PalindromeContext context = new PalindromeContext();
-        if (choice == 1) {
-            context.setStrategy(new StackStrategy());
-        } else {
-            context.setStrategy(new DequeStrategy());
-        }
-        boolean result = context.execute(input);
 
-        if (result)
-            System.out.println("The string is a Palindrome.");
-        else
-            System.out.println("The string is NOT a Palindrome.");
+        long start1 = System.nanoTime();
+        boolean result1 = twoPointer(input);
+        long end1 = System.nanoTime();
+
+        long start2 = System.nanoTime();
+        boolean result2 = stackMethod(input);
+        long end2 = System.nanoTime();
+
+        long start3 = System.nanoTime();
+        boolean result3 = recursiveMethod(input);
+        long end3 = System.nanoTime();
+
+        System.out.println("\n===== Performance Comparison =====");
+        System.out.println("Two Pointer Time: " + (end1 - start1) + " ns");
+        System.out.println("Stack Method Time: " + (end2 - start2) + " ns");
+        System.out.println("Recursive Time: " + (end3 - start3) + " ns");
 
         sc.close();
     }
